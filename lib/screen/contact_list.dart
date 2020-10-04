f473a6bc6bebaaa256ceb3d84258273f6eaf8469
BernardoLS <bernardolagunasantos@gmail.com>
2020-10-04 02:38:12 -0300

    feat: init Commit

---
+++ lib/screen/contact_list.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ContactList extends StatefulWidget {
+  @override
+  _ContactListState createState() => _ContactListState();
+}
+
+class _ContactListState extends State<ContactList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: ListTile(
+                title: Text(
+                  'Alex',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                  ),
+                ),
+                subtitle: Text(
+                  '1000',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { },
+        child: Icon(Icons.add)
+      ),
+    );
+  }
+}
