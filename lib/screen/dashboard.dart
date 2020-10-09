46842fba5429a4a53db3563cc69756d9def3c652
Bernardo Laguna <bernardo.laguna@taglivros.com.br>
2020-10-09 11:59:34 -0300

    feat: add a new item in dashboard and refactor itens to be a row with a new component called FeatureItem that represents an item in the dashboard.

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -16,45 +16,78 @@ class Dashboard extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Image.asset('images/bytebank_logo.png'),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Material(
-                color: Theme.of(context).primaryColor,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      (MaterialPageRoute(
-                        builder: (context) => ContactList(), 
-                      ))
-                    );
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(8.0),
-                    height: 100,
-                    width: 150,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Icon(
-                          Icons.people,
-                          color: Colors.white,
-                          ),
-                        Text(
-                          'Contacts',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.0,
-                          ),
-                          )
-                      ],
-                    )
-                  ),
+            Row(
+              children: [
+                _FeatureItem(
+                  'Transfer',
+                  Icons.monetization_on, 
+                  onClick: () {
+                    _showContactList(context);
+                  }
                 ),
-              ),
+                _FeatureItem(
+                  'Transaction Feed',
+                  Icons.description,
+                  onClick: () {
+                    _showContactList(context);
+                  },
+                )
+              ],
             )
           ]
         ),
     );
   }
+}
+
+class _FeatureItem extends StatelessWidget {
+  
+  final String title;
+  final IconData icon;
+  final Function onClick;
+
+  const _FeatureItem(this.title, this.icon, {@required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Material(
+                    color: Theme.of(context).primaryColor,
+                    child: InkWell(
+                      onTap: () => onClick(),
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        height: 100,
+                        width: 150,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(
+                              icon,
+                              color: Colors.white,
+                              ),
+                            Text(
+                              title,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                              ),
+                              )
+                          ],
+                        )
+                      ),
+                    ),
+                  ),
+                );
+  }
+}
+
+void _showContactList(BuildContext context) {
+  Navigator.of(context).push(
+    (MaterialPageRoute(
+      builder: (context) => ContactList(), 
+    ))
+  );
 }
